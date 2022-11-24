9a9b1847e36cd56cab146d870357848eac38612c
Leonardo do Valle <107518514+leonardovalle34@users.noreply.github.com>
2022-11-24 14:41:21 -0300

    3 deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require ("express");
 const app = express();
-/*const port = process.env.PORT || 3500;
 const  cors = require('cors');
 
 app.use(cors());
@@ -13,15 +12,5 @@ app.get("/downloadeng", (req,res)=>{
     res.download(__dirname + '/Public/CV2023devLeonardoValleEng.pdf')
 });
 
-app.listen(port, ()=>console.log(`http://localhost:${port}`))*/
+app.listen(3333, ()=>console.log(`Servidor rodando`))
 
-app.get("/",(req,res)=>{
-    try{
-        console.log("sucesso")
-    }catch(err){
-        console.log(err)
-    }
-    res.send("server is up teste");
-})
-
-app.listen(3333, ()=>console.log("Servidor rodando"))
